63edd4c03f28ab1c709d00f6c670f03a6ac32d82
Aubrey Zhang <138344000+auzhangLABS@users.noreply.github.com>
2023-09-06 15:52:03 -0400

    Update ssh_scp.sh

--- ssh_scp.sh
+++ ssh_scp.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-
+##this script will only work if package SSHPass is downloaded. 
+#creating a page that allows the users to select SSH or SCP
 echo '1 - ssh'
 echo '2 - scp'
 echo 'Select an option to continue'
-read varname
+read varname #reading the user input
 
+#creating a condition statement to see if it equal 1 or 2
+#if the user want ssh then it would start this if loop
 if [[ $varname -eq 1 ]] 
 then
         echo 'Please enter remote system ip address'
@@ -13,7 +16,9 @@ then
         read username
         echo $username'@'$ipaddress"'s password:"
         read -s password
+        #this would ssh with the password into another user
         sshpass -p $password ssh $username@$ipaddress
+#if user want SCP then this would start        
 elif [[ $varname -eq 2 ]] 
 then
         echo 'Please enter remote system ip address'
@@ -24,6 +29,7 @@ then
         echo -e '2 - remote \U2192 local'
         echo 'Please choose an option'
         read option
+        
         if [[ $option -eq 1 ]]
         then 
                 echo 'Please enter source file location and file name'
@@ -42,6 +48,8 @@ then
                 echo $username'@'$ipaddress"'s password:"
                 read -s password
                 scp $username'@'$ipaddress':'sfile dfile 
+        else 
+                echo 'N/A'
         fi
 else 
         echo 'N/A'
